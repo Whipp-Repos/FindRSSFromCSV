bb30d69cd446bc6498962beab1a85d897ced67de
Whipp <Whipppppp@protonmail.com>
2020-12-09 18:34:44 -0600

    Added some default possibilities and some output for what is happening when

--- FindRSSFromCSV.py
+++ FindRSSFromCSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import urllib
 import requests as req
 import feedparser as fp
 import sys
@@ -13,6 +14,29 @@ from urllib.parse import urljoin
 
 rssApplicationTypes = ['application/rss+xml', 'application/atom+xml',
                        'application/xml', 'text/xml']
+
+rssSubDomainOptions = ['feeds']
+
+rssDefaultPathPossibilities = ['/feed', '/rss.xml',
+                               '/xml/rss/all.xml', '/rss', '.rss',
+                               '/xml/rss.xml',
+                               '/xml/rss/rss.xml',
+                               '/']
+
+rssDomainSpecificPathPossibilities = [
+    {
+        "Domain": "tumblr.com",
+        "Path": "{domain}/rss"
+    },
+    {
+        "Domain": "blogspot.com",
+        "Path": "{domain}/feeds/posts/default"
+    },
+    {
+        "Domain": "medium.com",
+        "Path": "{domain}/feed/{path_0}"
+    }
+]
 
 if len(sys.argv) < 3:
     message = ('FindRssFromCSV.py <path-to-csv> '
@@ -61,11 +85,79 @@ for url in urlList:
     if urlParsed.netloc == '':
         print('No domain name, skipping...')
         continue
+
+    # Check if it is a known pattern and skip the rest of the checks.
+    domainName = '.'.join(urlParsed.netloc.split('.')[::-1][:2][::-1])
+    domainNamePossibility = [item for item
+                             in rssDomainSpecificPathPossibilities
+                             if item["Domain"] == domainName]
+
+    if domainNamePossibility:
+        for item in domainNamePossibility:
+            pathParsed = item["Path"]
+            path_0 = urlParsed.path.lstrip('/').split('/')[0]
+            defaultPath = pathParsed.format(
+                domain="https://" + urlParsed.netloc,
+                path_0=path_0)
+            foundRSSFeeds.append([url, defaultPath])
+        continue
+
+    # Build Possible Default values
+    domainNamesForDefaults = [domainName]
+    for subDomain in rssSubDomainOptions:
+        domainNamesForDefaults.append(
+            '{subDomain}.{domainName}'.format(
+                subDomain=subDomain, domainName=domainName))
+
+    defaultPossibilities = []
+    for domainNameForDefaults in domainNamesForDefaults:
+        for defaultPath in rssDefaultPathPossibilities:
+            fullDomainName = "https://{domain}".format(
+                domain=domainNameForDefaults)
+            basePath = urlParsed.path.rstrip('/')
+
+            rssPathPossibility = urljoin(
+                fullDomainName,
+                defaultPath)
+            defaultPossibilities.append(rssPathPossibility)
+
+            if urlParsed.path != '' and urlParsed.path != '/':
+                defaultPath = defaultPath.lstrip('/')
+                rssFullpathPossibility = urljoin(
+                    fullDomainName,
+                    basePath)
+                rssFullpathPossibility = urljoin(
+                    rssFullpathPossibility,
+                    defaultPath)
+                defaultPossibilities.append(rssFullpathPossibility)
+
+    defaultPossibilities = list(set(defaultPossibilities))
+
+    print(('Built {0} Default Link'
+           ' Possibilities').format(len(defaultPossibilities)))
+
+    defaultPossibilities = [[url, item] for item in defaultPossibilities]
+    foundRSSFeeds = foundRSSFeeds + defaultPossibilities
+
+    print(('{0} RSS Link'
+           ' Possibilities').format(len(foundRSSFeeds)))
+
+    print(('Requesting {0}'.format(url)))
     resp = req.get(url)
-    soup = bs(resp.text, 'html.parser')
+    baseDomain = "https://{domain}".format(domain=domainName)
+    print(('Requesting {0}'.format(baseDomain)))
+    respDomain = req.get(baseDomain)
+
+    soupPath = bs(resp.text, 'html.parser')
+    print(('Parsed {0}'.format(url)))
+    soupDomain = bs(respDomain.text, "html.parser")
+    print(('Parsed {0}'.format(baseDomain)))
 
     # Look for links on page that are called out as RSS Feeds
-    links = soup.find_all('link')
+    linksPath = soupPath.find_all('link')
+    linksDomain = soupDomain.find_all('link')
+    links = linksPath + linksDomain
+    print(('Found {0} links').format(len(links)))
     for link in links:
         try:
             linkType = link['type']
@@ -83,10 +175,16 @@ for url in urlList:
                 linkHref = urljoin(url, linkHref)
             foundRSSFeeds.append([url, linkHref])
 
+    print(('{0} RSS Link'
+           ' Possibilities').format(len(foundRSSFeeds)))
+
     # Follow local links on page to see if they return a valid application Type
-    alinks = soup.find_all('a')
+    alinksPath = soupPath.find_all('a')
+    alinksDomain = soupDomain.find_all('a')
+    alinks = alinksPath + alinksDomain
+
+    print(('Found {0} anchor links').format(len(alinks)))
     localLinks = []
-    alinks = []
     for alink in alinks:
         try:
             href = alink['href']
@@ -115,8 +213,14 @@ for url in urlList:
         if hrefParsed.netloc == urlParsed.netloc:
             if href not in localLinks:
                 localLinks.append(href)
+
+    localLinks = list(set(localLinks))
+
+    print(('Found {0} local links').format(len(localLinks)))
+
     for link in localLinks:
-        linkResp = req.get(link)
+        print(('Visiting {0}').format(link))
+        linkResp = req.head(link)
         try:
             linkRespContentType = linkResp.headers['content-type']
         except KeyError:
@@ -125,9 +229,22 @@ for url in urlList:
         if applicationType in rssApplicationTypes:
             foundRSSFeeds.append([url, link])
 
+    print(('{0} RSS Link'
+           ' Possibilities for {1}').format(len(foundRSSFeeds), url))
+
 validatedRssFeeds = []
+print
 for rssFeed in foundRSSFeeds:
-    feed = fp.parse(rssFeed[1])
+    print(('Checking {0} for actual Feed').format(rssFeed[1]))
+    try:
+        feed = fp.parse(rssFeed[1])
+    except ConnectionRefusedError:
+        print("Rejected")
+        continue
+    except urllib.error.URLError:
+        print ("Rejected")
+        continue
+
     try:
         title = feed['feed']['title']
     except KeyError:
